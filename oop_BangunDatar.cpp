f2fde4d0461102081ed9fcc2d4ec74d0a8e3bc44
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-23 15:42:36 +0700

    menambahkan member methode display pada class bangunDatar

--- oop_BangunDatar.cpp
+++ oop_BangunDatar.cpp
@@ -16,4 +16,9 @@ class bangunDatar {
         float hitungLuas() {
             return panjang * lebar;
         }
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
 };
